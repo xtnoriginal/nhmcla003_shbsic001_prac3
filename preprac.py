aaa8768273c46fbe7b6de781a4653093757bc3bd
nhmcla003 <nhmcla003@myuct.ac.za>
2020-09-22 22:23:23 +0100

    The led can be switched on and off and the programming has passed testing

--- preprac.py
+++ preprac.py
@@ -1,3 +1,7 @@
+#  SHBSIC001 and NHMCLA003
+#  Turning on a LED ON and OFF
+#  EEE2095S
+
 #importing GPIO for input and time for delay
 import RPi.GPIO as GPIO
 from time import sleep
@@ -9,25 +13,38 @@ GPIO.setmode(GPIO.BOARD)
 #switch off all  warnings 
 GPIO.setwarnings(False)
 
+#state variable to indicate the current sate of the LED
+
+
+def my_callback(channel):
+	#check current state of the LED
+	if GPIO.input(8)==GPIO.LOW:
+		#Turn on the LED
+		GPIO.output(8,GPIO.HIGH)
+		state=1
+	
+	else:
+		#Switch off the LED
+		GPIO.output(8,GPIO.LOW)
+		state=0
 
 def main():
+	#Setup the OUTPUT pin for the Led and set default to low
 	GPIO.setup(8,GPIO.OUT,initial=GPIO.LOW)
+
+	#SETUP input pin  and  use a PULL DOWN resitor as  we want to set the default input to 0
 	GPIO.setup(37,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+	#Detect when the user presses a button and  avoid switch debouncing
+	GPIO.add_event_detect(37, GPIO.RISING, callback=my_callback, bouncetime=200)
 	try:
-		state=0;
+		#loop  indefinetly until the user cancels the program
 		while True:
-			
-			if GPIO.input(37):
-		
-				if state==0:
-					GPIO.output(8,GPIO.HIGH)
-					state=1
-				else:
-					GPIO.output(8,GPIO.LOW)
-					state=0
+			sleep(1)
 			
 			
 	except KeyboardInterrupt:
+		#cleanup all pins used
         	GPIO.cleanup()	
 
 
